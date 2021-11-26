634e480de254bdf139f6f205ecc3a9d14d33e92a
Dmytro Semeruk <deemoon83@gmail.com>
2021-11-26 09:22:40 +0200

    tel

--- src/Components/ContactItem/ContactItem.js
+++ src/Components/ContactItem/ContactItem.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 
 export default function ContactItem({ name, number, id }) {
   const dispatch = useDispatch();
+  const tel = `tel:${number}`;
   // const { contacts__item, contact__name, contact__number, contacts__btn } =
   //   styles;
   return (
@@ -30,7 +31,6 @@ export default function ContactItem({ name, number, id }) {
 
         <div className="card-body">
           <p className="card-text d-flex justify-content-start align-items-center">
-            {" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -61,19 +61,21 @@ export default function ContactItem({ name, number, id }) {
           </p>
           <div className="d-flex justify-content-between align-items-center">
             <button type="button" className="btn btn-sm btn-outline-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="black"
-                className="bi bi-telephone-forward-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm10.761.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708z"
-                />
-              </svg>
+              <a href={tel}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="black"
+                  className="bi bi-telephone-forward-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm10.761.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708z"
+                  />
+                </svg>
+              </a>
             </button>
             {/* <div className="btn-group">
               <button
